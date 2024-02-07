a04e78499d3adafe90e529e65e013f2b0cb63efc
IKshv <ikshvartsman@gmail.com>
2024-02-07 00:45:29 -0800

    a

--- utils/download.py
+++ utils/download.py
@@ -5,21 +5,13 @@ import time
 from utils.response import Response
 
 def download(url, config, logger=None):
-    if config.cache_server is None:
-        raise ValueError("Cache server details are not set in config.")
     host, port = config.cache_server
-    # print(f"Downloading {url} using cache {host}:{port}")
-    req_args={
-        'url' : f"http://{host}:{port}/",
-        'params' : [("q", f"{url}"), ("u", f"{config.user_agent}")]
-    }
-    # print(req_args)
-    resp = requests.get(**req_args, timeout=5)
+    resp = requests.get(
+        f"http://{host}:{port}/",
+        params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
     try:
         if resp and resp.content:
-            a = Response(cbor.loads(resp.content))
-            # print(a)
-            return a
+            return Response(cbor.loads(resp.content))
     except (EOFError, ValueError) as e:
         pass
     logger.error(f"Spacetime Response error {resp} with url {url}.")
--- utils/response.py
+++ utils/response.py
@@ -5,15 +5,10 @@ class Response(object):
         self.url = resp_dict["url"]
         self.status = resp_dict["status"]
         self.error = resp_dict["error"] if "error" in resp_dict else None
-        print("")
         try:
             self.raw_response = (
                 pickle.loads(resp_dict["response"])
                 if "response" in resp_dict else
                 None)
-            if self.raw_response:
-                print(len(self.raw_response.content))
-            if not self.raw_response or len(self.raw_response.content) > 50000:
-                self.raw_response = None
         except TypeError:
             self.raw_response = None
